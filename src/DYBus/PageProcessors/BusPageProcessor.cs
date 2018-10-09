35b77938aef2b76f6f66ba774e4bd50b536fd948
Crashsol <47147551@qq.com>
2018-10-09 17:58:28 +0800

    创建爬虫项目

---
+++ src/DYBus/PageProcessors/BusPageProcessor.cs
@@ -0,0 +1,19 @@
+﻿using DotnetSpider.Core;
+using DotnetSpider.Core.Processor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DYBus.PageProcessors
+{
+    /// <summary>
+    /// 对获取到的网页，进行解析，解析到的数据，将传到 Pipeline 中，进行处理
+    /// </summary>
+    class BusPageProcessor : BasePageProcessor
+    {
+        protected override void Handle(Page page)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
