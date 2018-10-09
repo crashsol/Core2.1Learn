28acdd22e57d6e20dabc3767788f5c753ac2bd64
crashsol <47147551@qq.com>
2018-10-09 21:02:50 +0800

    获取所有车站信息

---
+++ src/DYBus/ProcessorAndPipeline/DYBusStationHandler.cs
@@ -0,0 +1,92 @@
+﻿using DotnetSpider.Extension;
+using DotnetSpider.Extension.Model;
+using DotnetSpider.Extension.Pipeline;
+using DotnetSpider.Extraction;
+using DotnetSpider.Extraction.Model.Attribute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DYBus.ProcessorAndPipeline
+{
+    public class DYBusStationHandler
+    {
+
+        public static void Run()
+        {
+            DYBusStationSpider dYBusStationSpider = new DYBusStationSpider();
+            dYBusStationSpider.Run();
+
+        }
+
+
+        /// <summary>
+        /// 定义一个爬虫获取所有车站信息
+        /// </summary>
+
+        private class DYBusStationSpider : EntitySpider
+        {
+            public DYBusStationSpider() { }
+
+            protected override void OnInit(params string[] arguments)
+            {
+               
+                foreach (var item in BusLines.Lines)
+                {
+                    AddRequest($"http://wapapp.dy4g.cn/bus/auto/test.php?t=linhtml&busline={item}", new Dictionary<string, dynamic> { { "Keyword",$"{item}公交线" } });
+                }
+            
+                AddEntityType<BusStation>();
+                AddPipeline(new ConsoleEntityPipeline());
+                AddPipeline(new SqlServerEntityPipeline("Server=.\\SQLEXPRESS;uid=sa;pwd=123qwe!@#;Database=DyBus;Trusted_Connection=True;MultipleActiveResultSets=true"));
+            }
+
+
+            /// <summary>
+            /// 获取车站信息
+            /// </summary>
+            [Schema("DYBus", "BusStation")]
+            [Entity(Expression = ".//div[@class='busstoplist']/div/p", Type = SelectorType.XPath)]
+            class BusStation : BaseEntity
+            {
+                /// <summary>
+                /// 车次信息
+                /// </summary>
+                [Column]
+                [Field(Expression = "Keyword", Type = SelectorType.Enviroment)]
+                public string Keyword { get; set; }
+
+                /// <summary>
+                /// 车站唯一ID
+                /// </summary>
+                [Column]
+                [Field(Expression = "./@Id")]
+                public string BusStationId { get; set; }
+
+
+                /// <summary>
+                /// 车次路线编号
+                /// </summary>
+                [Column]
+                [Field(Expression = "./strong/text()")]
+                public string StationNumber { get; set; }
+
+                /// <summary>
+                /// 车站名称
+                /// </summary>
+                [Column]
+                [Field(Expression = "./span/text()")]
+                public string Name { get; set; }
+
+                /// <summary>
+                /// 车站方向
+                /// </summary>
+                [Column]
+                [Field(Expression = "../@class")]
+                public string BusDirection { get; set; }
+
+            }
+
+        }
+    }
+}
--- src/DYBus/Program.cs
+++ src/DYBus/Program.cs
@@ -4,8 +4,7 @@ using DotnetSpider.Core.Pipeline;
 using DotnetSpider.Core.Processor;
 using DotnetSpider.Core.Scheduler;
 using DotnetSpider.Downloader;
-using DYBus.PageProcessors;
-using DYBus.Pipelines;
+using DYBus.ProcessorAndPipeline;
 using System;
 
 
@@ -15,34 +14,13 @@ namespace DYBus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
 
-        private static void BusPageProcessorAndPipeline() 
-        {
-            // 玄幻小说的首页网址
+            //获取所有车站信息
+            DYBusStationHandler.Run();
+            Console.WriteLine("-----------------------------------------------");
 
-            string url = "http://www.jjxsw.com/e/action/ListInfo.php?page=0&classid=11&line=10&tempid=3&ph=1&andor=and&qujian=4&orderby=2&myorder=0&totalnum=150";
 
-            Site site = new Site { CycleRetryTimes = 3, SleepTime = 300 };
-
-            site.AddStartUrl(url);
-
-            // 使用 PageProcessor和 Scheduler创建一个爬行器。添加数据处理管道
-
-            Spider spider = Spider.Create(site, new QueueDuplicateRemovedScheduler(), new BusPageProcessor()).AddPipeline(new BusPipeline());
-
-            // 爬虫下载器
-            spider.Downloader = new HttpClientDownloader();
-
-            // 爬虫的线程数
-            spider.ThreadNum = 4;
-
-            // 当没有其它链接请求时，爬虫的退出时间
-            spider.EmptySleepTime = 2000;
-
-            // 启动爬虫
-            spider.Run();
+            Console.ReadKey();
         }
     }
 }
