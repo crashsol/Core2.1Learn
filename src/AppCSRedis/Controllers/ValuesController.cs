780c7864633ea49e3ad9d9e37d137714b6a478f8
Crashsol <47147551@qq.com>
2018-10-27 11:12:02 +0800

    添加HangFire，CSRedis,IdentityWithMysql

---
+++ src/AppCSRedis/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace AppCSRedis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly IDistributedCache _redis;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _redis = distributedCache;
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+
+            //使用以来注入读取Redis数据
+            //IDistributedCache 存储数据默认使用的Hash
+            _redis.SetString("message","少时诵诗书所所所所所所所所所所所所所所所");
+            var message = _redis.GetString("message");
+
+
+            //使用CSRedis提供的静态方法
+            var bus23 = RedisHelper.HGetAll("23");
+
+            
+            var message2 = RedisHelper.HGet("message","data");
+
+
+            return new string[] { message, Newtonsoft.Json.JsonConvert.SerializeObject(bus23), message2 };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/HangfireApp/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HangfireApp.Models;
+using Hangfire;
+
+namespace HangfireApp.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+       
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult AddJob()
+        {
+            var now = DateTime.Now;
+            //后台执行一个任务
+            BackgroundJob.Enqueue(() => Console.WriteLine($"Add job {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff")}"));
+            //后台添加一个计划任务
+            BackgroundJob.Schedule(() => Console.WriteLine($"30 秒后开始执行, Job Excete At {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff")}"), TimeSpan.FromSeconds(30));
+            return Ok("添加任务成功");
+        }
+    }
+}
