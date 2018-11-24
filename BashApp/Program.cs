86d6395253c965f6179609f9e68a4547ea5563ef
Crashsol <47147551@qq.com>
2018-11-24 10:00:33 +0800

    1

---
+++ BashApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace BashApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "/home/test/test/";
+            string fileUrl = "/home/wwwroot/publish/BashApp.deps.json";
+
+            try
+            {
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+
+                Console.WriteLine("开始拷贝");
+
+                File.Copy(fileUrl, filePath, true);
+
+                Console.WriteLine("拷贝完成");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        
+    }
+}
