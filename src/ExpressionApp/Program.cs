76b1224f0467347a05ee44adb457f02cb6483aa3
crashsol <47147551@qq.com>
2019-01-04 14:57:11 +0800

    2

---
+++ src/ExpressionApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+
+
+namespace ExpressionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Expression<Func<int, int, bool>> expression;
+            expression = (x, y) => x > y;
+            Console.WriteLine($"---------------{expression}---------------------{System.Environment.NewLine}");
+
+            
+            Console.WriteLine(); 
+            Console.WriteLine();
+
+            Console.Read();
+        }
+
+       
+    }
+}
